e49d251a0d5c5baa3125703b5ee5a598a106dba5
nuits.jp <nuits.jp@live.jp>
2018-02-26 14:26:16 +0900

    従業員一覧画面の追加

---
+++ Source/EmployeeManager/EmployeeManager.Presentation/Views/ManagedEmployeeList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AdventureWorks.EmployeeManager.Presentation.Views
+{
+    /// <summary>
+    /// Interaction logic for ManagedEmployeeList
+    /// </summary>
+    public partial class ManagedEmployeeList : UserControl
+    {
+        public ManagedEmployeeList()
+        {
+            InitializeComponent();
+        }
+    }
+}
